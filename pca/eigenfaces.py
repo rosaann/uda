559eea056981fb4c022d65d25ef90cce71cdb5e4
zengling <zengling@sengled.com>
2017-12-08 18:39:02 +0800

    pca study

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -38,19 +38,21 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 ###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-
+#print "xxxxxxx ", lfw_people
 # introspect the images arrays to find the shapes (for plotting)
 n_samples, h, w = lfw_people.images.shape
+
 np.random.seed(42)
 
 # for machine learning we use the data directly (as relative pixel
 # position info is ignored by this model)
 X = lfw_people.data
 n_features = X.shape[1]
-
+#print "data shape ", X.shape
 # the label to predict is the id of the person
 y = lfw_people.target
 target_names = lfw_people.target_names
+#print "target name ", target_names
 n_classes = target_names.shape[0]
 
 print "Total dataset size:"
@@ -66,14 +68,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
-
+n_components = 120
+#pl.imshow(X_train[100].reshape((h, w)), cmap=pl.cm.gray)
+#pl.show()
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
 
+print "components__ len ", pca.components_.shape
 eigenfaces = pca.components_.reshape((n_components, h, w))
+
 
 print "Projecting the input data on the eigenfaces orthonormal basis"
 t0 = time()
@@ -81,7 +86,7 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
 
-
+print "X_train_pca ",X_train_pca.shape, " X_train ", X_train.shape
 ###############################################################################
 # Train a SVM classification model
 
